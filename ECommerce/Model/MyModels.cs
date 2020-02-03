61ae63d5c89021cdb1d65e01d2905b8e1020425b
Dimitris <iracleous@gmail.com>
2020-02-03 16:24:57 +0200

    Add project files.

---
+++ ECommerce/Model/MyModels.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ECommerce.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+         
+        public override string ToString()
+        {
+            return $"Name= {Name} Price= {Price}";
+        }
+
+   
+    } 
+    public class Order
+    {
+        public List<Product> Products { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime Date { get; set; }
+
+        public void Print()
+        {
+           foreach(Product product in Products)
+            {
+                Console.WriteLine(product.ToString());
+            }
+        }
+        public void Print1()
+        {
+            Products.ForEach(product => Console.WriteLine(product));
+        }
+       
+
+
+    } 
+}
---
+++ ECommerce/Program.cs
@@ -0,0 +1,104 @@
+﻿using ECommerce.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace ECommerce
+{
+    class Program
+    {
+        static string s1 = "Server =localhost; Database =accenture; Integrated Security=SSPI;Persist Security Info=False;"; 
+        static string s2 = "Server =localhost; Database =accenture; Persist Security Info=True;User ID=sa;Password=passw0rd;";
+
+ 
+
+        static void Main(string[] args)
+        {
+            Product product = new Product
+            {
+                Name = "education service",
+                Price = 11.90m
+            };
+            test4(product);
+            test2();
+        }
+
+
+
+
+        public static void test3()
+        {
+
+            int insertedItems = 0;
+            using SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = s2;
+            conn.Open();
+            SqlCommand insertCommand = new SqlCommand(
+                "INSERT INTO product (name, price) VALUES (@0, @1)", conn);
+            insertCommand.Parameters.Add(  new SqlParameter("0", "radikia"));
+            insertCommand.Parameters.Add(  new SqlParameter("1", 1.89));
+            insertedItems = insertCommand.ExecuteNonQuery();
+        }
+
+        public static void test4(Product p)
+        {
+
+            int insertedItems = 0;
+            using SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = s2;
+            conn.Open();
+            SqlCommand insertCommand = new SqlCommand(
+                "INSERT INTO product (name, price) VALUES (@0, @1)", conn);
+            insertCommand.Parameters.Add(new SqlParameter("0", p.Name));
+            insertCommand.Parameters.Add(new SqlParameter("1", p.Price));
+            insertedItems = insertCommand.ExecuteNonQuery();
+        }
+
+
+        public static void test2()
+        {
+            using SqlConnection conn = new SqlConnection
+            {
+                ConnectionString = s2
+            };
+            conn.Open();
+            SqlCommand command = new SqlCommand("SELECT * from product", conn);
+            using SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine("FirstColumn\tSecond Column");
+            while (reader.Read())
+            {
+                Console.WriteLine(
+                $"{reader[0]} \t | {reader[1]} \t  | {reader[2]} \t");
+            }
+        }
+
+
+        public static void test()
+        {
+            Product product = new Product
+            {
+                Name = "chocolates",
+                Price = 20
+            };
+
+            Customer customer = new Customer
+            {
+                FirstName = "Dimitris",
+                LastName = "Iracleous",
+                Age = 18
+            };
+
+            Order order = new Order
+            {
+                Customer = customer,
+                Products = new List<Product>(),
+                Date = DateTime.Now
+            };
+
+            order.Products.Add(product);
+
+            order.Print();
+
+        }
+    }
+}
